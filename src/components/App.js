5576ed779ab8f2df55bb0c40d4206549e5707132
mcnadams <mcneilbo@oregonstate.edu>
2019-05-20 11:24:54 -0700

    app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Header from './Header';
+import Red from './Red';
+import Blue from './Blue';
+import Yellow from './Yellow';
 
 export default function App() {
   return (
     <Router>
-      {/* stuff */}
+      <>
+        <Header />
+        <Switch>
+          <Route path='/red' component={Red} />
+          <Route path='/blue' component={Blue} />
+          <Route path='/yellow' component={Yellow} />
+        </Switch>
+      </>
     </Router>
-  )
+  );
 }
